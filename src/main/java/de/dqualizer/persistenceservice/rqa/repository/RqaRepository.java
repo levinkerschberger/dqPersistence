2be7d9ae4cd5908b4505b0cdeedd034b43b41ee8
Levin <Levin.Kerschberger@novatec-gmbh.de>
2023-06-28 14:16:31 +0200

    repository

--- src/main/java/de/dqualizer/persistenceservice/rqa/repository/RqaRepository.java
+++ src/main/java/de/dqualizer/persistenceservice/rqa/repository/RqaRepository.java
@@ -2,11 +2,12 @@ package de.dqualizer.persistenceservice.rqa.repository;
 
 import de.dqualizer.persistenceservice.rqa.model.Rqa;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface RqaRepository extends MongoRepository<Rqa, String> {
     public Rqa findById(int Id);
-    public List<Rqa> findByName(String name);
+    public List<Rqa> findByName(@Param("name") String name);
 
 }
