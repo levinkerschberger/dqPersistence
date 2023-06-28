353b5f98640b98178579c1a9fa7badb10b779545
maeddes <maeddes@gmail.com>
2023-06-28 13:30:34 +0000

    mongo config

--- src/main/java/de/dqualizer/persistenceservice/rqa/repository/RqaRepository.java
+++ src/main/java/de/dqualizer/persistenceservice/rqa/repository/RqaRepository.java
@@ -1,19 +1,20 @@
 package de.dqualizer.persistenceservice.rqa.repository;
 
-import de.dqualizer.persistenceservice.rqa.model.Rqa;
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.repository.query.Param;
-import java.util.List;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-import java.util.List;
+import de.dqualizer.persistenceservice.rqa.model.Rqa;
 
+@RepositoryRestResource
 public interface RqaRepository extends MongoRepository<Rqa, String> {
-    @Query("{id: '?ß'}")
-    public Rqa findById(int Id);
+    // @Query("{id: '?ß'}")
+    // public Rqa findById(int Id);
 
-    @Query("{name:'?0'}")
-    public List<Rqa> findByName(@Param("name") String name);
+    // @Query("{name:'?0'}")
+    // public List<Rqa> findByName(@Param("name") String name);
 
 }
